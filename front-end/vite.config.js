429c6909c4ea273b9958a729d07dfc42aea0715b
Varshini Shree <117148310+VarshiniShreeV@users.noreply.github.com>
2025-06-14 06:25:08 +0530

    Fix: Deployment issues

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const isGitHubPages = process.env.GITHUB_PAGES === 'true'
-
+// https://vite.dev/config/
 export default defineConfig({
-  base: isGitHubPages ? '/Portfolio/' : '/',
+  base: '/Portfolio/',
   plugins: [react()],
 })
