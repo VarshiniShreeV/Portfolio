02324b80b3bc8087005215e5c76f91889be51823
Varshini Shree <117148310+VarshiniShreeV@users.noreply.github.com>
2025-06-20 15:30:25 +0530

    Updates

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-const isGitHubPages = process.env.GITHUB_PAGES === 'true'
-
-export default defineConfig({
-  base: isGitHubPages ? '/Portfolio/' : '/',
-  plugins: [react()],
-})
